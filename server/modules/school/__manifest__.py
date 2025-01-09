6711567654ffbf05bb4e18f2e6a2ab01f1e6fb69
XuanHuyen <goat2625@gmail.com>
2025-01-09 10:36:52 +0700

    odoo 17

---
+++ server/modules/school/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': "School",
+    'version': '1.0',
+    'summary': 'School Management',
+    'category': 'School',
+    'sequence': 1,
+    'author': 'Nxhuyen',
+    'website': 'https://www.nxhuyen.com',
+    'description': """
+        App quản lý trường học
+    """,
+    'depends': [''],
+    'data': [],
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
